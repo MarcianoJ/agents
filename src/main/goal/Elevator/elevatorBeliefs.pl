9e5b94eaeef29bb3dfea85ca87b3221252b9c1ac
Wouter1 <W.Pasman@TUDelft.nl>
2015-09-02 15:37:15 +0200

    #3630 re-committing, again adding files

---
+++ src/main/goal/Elevator/elevatorBeliefs.pl
@@ -0,0 +1,6 @@
+% The direction that will be shown to the people at the next stop.
+% This remains the intended direction of movement until there are no more
+% planned stops in that direction.
+dir(down).
+% Initially, it is unknown whether doors are open or closed.
+doorState(unknown).
---
+++ src/main/goal/Elevator/elevatorGoals.pl
@@ -0,0 +1,2 @@
+atFloor(1), dir(up).
+

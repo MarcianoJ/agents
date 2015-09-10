4fb60e9115fc6625ab1c147e93b0578ae6ebb0e3
Wouter1 <W.Pasman@TUDelft.nl>
2015-09-10 10:42:19 +0200

    #3630 fixed transport

---
+++ src/main/goal/Transport/transport.pl
@@ -0,0 +1,17 @@
+:-dynamic(order/2).
+:-dynamic(in/2).
+:-dynamic(loc/2).
+:-dynamic(home/1).
+
+
+ordered(C,P) :- order(C,Y), member(P,Y).
+loaded_order(C) :- order(C,O), loaded(O).
+loaded([P]) :- in(P, truck).
+loaded([P|L]) :- in(P,truck), loaded(L).
+empty :- not(in(P,truck)).
+%packed :- in(P, truck), in(P1, truck), not(P=P1).
+packed :- setof(P,in(P,truck),L), length(L,X), X>=2.
+%delivered_order(C) :- order(C,O),loc(C,X),orderloc(O,X),loc(truck,a).
+delivered_order(C) :- order(C,O),loc(C,X),orderloc(O,X).
+orderloc([H|T], X) :- loc(H,X), orderloc(T,X).     
+orderloc([], X). 

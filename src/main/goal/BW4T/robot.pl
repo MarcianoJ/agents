c044db12bc6e950f3cfa639f9cabbf82a8923992
Wouter1 <W.Pasman@TUDelft.nl>
2016-02-18 16:43:51 +0100

    #2970 fixes for new grammar

--- src/main/goal/BW4T/robot.pl
+++ src/main/goal/BW4T/robot.pl
@@ -1,5 +1,9 @@
 :-dynamic state/1, in/1, zone/5.
 
+:-dynamic ownId/1.    % our BW4T ID.
+
+:-dynamic associate/2.%associated GOAL agent name to BW4T ID.
+
 % A room is a place with exactly one neighbour, i.e., there is only one way to get to and from that place.
 room(PlaceID) :- zone(_,PlaceID,_,_,Neighbours), length(Neighbours,1).
 

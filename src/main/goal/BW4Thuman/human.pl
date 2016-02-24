8c48b990f74b43a9192e596b64476c7862a1946d
Vincent Koeman <vj.koeman@quicknet.nl>
2016-02-24 16:06:53 +0100

    oops forgot to add the new folders

---
+++ src/main/goal/BW4Thuman/human.pl
@@ -0,0 +1,5 @@
+:-dynamic performGoTo/1, performGoTo/2, 
+		performGoToBlock/1, performGoToBlock/2
+		performPickUp/0, performPutDown/0,
+		ownId/1,     % our BW4T ID.
+		associate/2. %associated GOAL agent name to BW4T ID.
---
+++ src/main/goal/Tower2/tower.pl
@@ -0,0 +1,13 @@
+:-dynamic holding/1, on/2, block/1, target/2, maintain/0.
+
+% Assume there is enough room to put all blocks on the table.
+% This is actually only true for up to 13 blocks in the Tower Environment.
+clear(table).
+clear(X) :- block(X), not( on(Y, X) ), not( holding(X) ).
+clear([X|_]) :- clear(X).
+
+above(X, Y) :- on(X, Y).
+above(X, Y) :- on(X, Z), above(Z, Y).
+
+tower([X]) :- on(X, table).
+tower([X, Y| T]) :- on(X, Y), tower([Y| T]).

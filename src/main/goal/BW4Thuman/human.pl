dc7b5a67d6ebc49b56e6f728817ee936548fa0f0
Wouter1 <W.Pasman@TUDelft.nl>
2016-02-25 11:33:36 +0100

    #3798 human working again.

--- src/main/goal/BW4Thuman/human.pl
+++ src/main/goal/BW4Thuman/human.pl
@@ -1,5 +1,9 @@
-:-dynamic performGoTo/1, performGoTo/2, 
-		performGoToBlock/1, performGoToBlock/2
-		performPickUp/0, performPutDown/0,
-		ownId/1,     % our BW4T ID.
-		associate/2. %associated GOAL agent name to BW4T ID.
+:-dynamic associate/2.
+:-dynamic ownId/1.
+
+:-dynamic performGoTo/1.
+:-dynamic performGoTo/2. 
+:-dynamic performGoToBlock/1.
+:-dynamic 	performGoToBlock/2.
+:-dynamic  performPickUp/1.
+:-dynamic performPutDown/0.

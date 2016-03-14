9cda03800a1d772093758e221457812c5355b764
Vincent Koeman <vj.koeman@quicknet.nl>
2016-03-14 23:31:13 +0100

    update coffee again; goal management in event module & don't just broadcast messages without a proper reason to do so (+commentfixes)

--- src/main/goal/CoffeeAgents/coffee.pl
+++ src/main/goal/CoffeeAgents/coffee.pl
@@ -1,6 +1,6 @@
-:-dynamic have/1, % indicates that we have a product ready for use
-	canProduce/1, % indicates what we can make ourselves
-	canProduce/2, % indicates that an other machine can make a certain product
+:-dynamic have/1, % indicates that we have a product ready for use.
+	canProduce/1, % indicates products that we can make ourselves.
+	canProduce/2, % indicates that another machine can make a certain product.
 	delivered/2. % indicates that a product has been delivered to a certain machine.
 
 % Common knowledge of ingredients that are needed for making a product.
@@ -9,9 +9,9 @@ requiredFor(coffee, grounds).
 requiredFor(espresso, coffee).
 requiredFor(grounds, beans).
 
-% A machine can make a Product if it has the product in stock or can make all ingredients.
+% A machine can make a product if it has the product in stock or can make all ingredients.
 canMake(Product) :- have(Product).
 canMake(Product) :- canProduce(Product), forall(requiredFor(Product, Ingredient), canMake(Ingredient)).
 
-% A short hand for having delivered a product to any machine
+% A short hand for having delivered a product to any machine.
 delivered(Product) :- delivered(_,Product).

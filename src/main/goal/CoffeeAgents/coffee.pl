0677bb779a78d7661de4c75fa689ebfb6e0f6b51
Vincent Koeman <vj.koeman@quicknet.nl>
2016-03-14 12:30:30 +0100

    improved coffee example

--- src/main/goal/CoffeeAgents/coffee.pl
+++ src/main/goal/CoffeeAgents/coffee.pl
@@ -1,21 +1,17 @@
-%%%%% Common knowledge of ingredients that are needed for making a coffee product. %%%%%
-:-dynamic me/1, % indicates who we are
-		have/1, % indicates we have a product ready for use
-		delivered/2. % delivered(Machine, product) indicates a product delivered to machine. 
+:-dynamic have/1, % indicates that we have a product ready for use
+	canProduce/1, % indicates what we can make ourselves
+	canProduce/2, % indicates that an other machine can make a certain product
+	delivered/2. % indicates that a product has been delivered to a certain machine.
 
+% Common knowledge of ingredients that are needed for making a product.
 requiredFor(coffee, water).
 requiredFor(coffee, grounds).
-requiredFor(espresso, coffee). 
+requiredFor(espresso, coffee).
 requiredFor(grounds, beans).
 
-% canProduce indicates what a machine can make (if it has the requiredFor).
-canProduce(grinder,grounds).
-canProduce(maker,coffee).
-canProduce(maker,expresso).
-
-% shortcut for what this machine can produce.
-canProduce(Product) :- me(Me), canProduce(Me, Product).
-
-% A Machine can make a Product if it has the product in stock or can make all ingredients.
+% A machine can make a Product if it has the product in stock or can make all ingredients.
 canMake(Product) :- have(Product).
 canMake(Product) :- canProduce(Product), forall(requiredFor(Product, Ingredient), canMake(Ingredient)).
+
+% A short hand for having delivered a product to any machine
+delivered(Product) :- delivered(_,Product).

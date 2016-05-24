f571fcdbed19de0b8272bd3201a137f3b77d89a3
Wouter1 <W.Pasman@TUDelft.nl>
2016-05-24 16:26:53 +0200

    #3902 now the correct files

--- src/main/goal/Tygron/dummy.pl
+++ src/main/goal/Tygron/dummy.pl
@@ -2,6 +2,7 @@
 :- dynamic stakeholders/1.
 :- dynamic settings/1.
 :- dynamic functions/1.
+:- dynamic lands/1.
 :- dynamic buildings/1.
 
 % we have a building if the building list has at least 1 element.

5219da41558dd90a1b4484c54332154ef2515c3a
Wouter1 <W.Pasman@TUDelft.nl>
2016-05-24 19:55:51 +0200

    #3902 tygron agent more changes. Still not working.

--- src/main/goal/Tygron/dummy.pl
+++ src/main/goal/Tygron/dummy.pl
@@ -4,6 +4,9 @@
 :- dynamic functions/1.
 :- dynamic lands/1.
 :- dynamic buildings/1.
+:- dynamic requests/1.
+:- dynamic offeredLand/0.
+:- dynamic soldLand/0.
 
 % we have a building if the building list has at least 1 element.
 havebuilding :- buildings([X|Y]).
